28b4f1eeac886137c50eefcedcf130a281d14971
André Miranda <affmde@hotmail.com>
2022-05-08 16:07:56 +0300

    controls increased size

--- js/gameScene.js
+++ js/gameScene.js
@@ -24,7 +24,7 @@ class GameScene extends Phaser.Scene{
         this.load.image('heart', 'assets/red-heart.png')
         this.load.image('ring', 'assets/ring.png');
         this.load.image('ArrowsController', 'assets/controls.png')
-        this.load.image('ControlA', 'assets/controlA.png');
+        this.load.image('ControlA', 'assets/ControlA.png');
         this.load.image('squareFake', 'assets/squareFakeControl.png')
     }
 
@@ -46,12 +46,12 @@ class GameScene extends Phaser.Scene{
         this.rings= this.physics.add.staticGroup();
         this.smallPlatforms= this.physics.add.staticGroup()
         this.platforms= this.physics.add.staticGroup()
-        arrows = this.add.image(100, 500, 'ArrowsController').setAlpha(0.7).setScrollFactor(0).setInteractive();
-        controlA = this.add.image(650,500, 'ControlA').setAlpha(0.5).setScale(0.6).setScrollFactor(0).setInteractive();
-        squareLeft = this.add.image(50, 500, 'squareFake').setScale(0.7).setInteractive().setScrollFactor(0).setAlpha(0.1);
-        squareRight = this.add.image(150, 500, 'squareFake').setScale(0.7).setInteractive().setScrollFactor(0).setAlpha(0.1);
-        squareUp = this.add.image(100, 450, 'squareFake').setScale(0.7).setInteractive().setScrollFactor(0).setAlpha(0.1);
-        squareDown = this.add.image(100, 550, 'squareFake').setScale(0.7).setInteractive().setScrollFactor(0).setAlpha(0.1);
+        arrows = this.add.image(120, 500, 'ArrowsController').setAlpha(0.7).setScrollFactor(0).setInteractive().setScale(1.5);
+        controlA = this.add.image(650,500, 'ControlA').setAlpha(0.5).setScale(0.8).setScrollFactor(0).setInteractive();
+        squareLeft = this.add.image(50, 500, 'squareFake').setScale(1).setInteractive().setScrollFactor(0).setAlpha(0.01);
+        squareRight = this.add.image(190, 500, 'squareFake').setScale(1).setInteractive().setScrollFactor(0).setAlpha(0.01);
+        squareUp = this.add.image(120, 430, 'squareFake').setScale(1).setInteractive().setScrollFactor(0).setAlpha(0.01);
+        squareDown = this.add.image(120, 570, 'squareFake').setScale(1).setInteractive().setScrollFactor(0).setAlpha(0.01);
         //Create Live Hearts
         for(let i = 0; i < gameStats.lives; i++){
             const heart = this.add.image(20 + i*30, 20, 'heart');
@@ -125,27 +125,27 @@ class GameScene extends Phaser.Scene{
         //Mobile Controls
         squareLeft.on('pointerdown', ()=>{
             this.player.setVelocityX(-160)
-            squareLeft.setAlpha(0.3)
+            squareLeft.setAlpha(0.03)
         })
         squareLeft.on('pointerup', ()=>{
             this.player.setVelocityX(0);
-            squareLeft.setAlpha(0.1)
+            squareLeft.setAlpha(0.01)
         })
         squareRight.on('pointerdown', ()=>{
             this.player.setVelocityX(160)
-            squareRight.setAlpha(0.3)
+            squareRight.setAlpha(0.03)
         })
         squareRight.on('pointerup', ()=>{
             this.player.setVelocityX(0)
-            squareRight.setAlpha(0.1)
+            squareRight.setAlpha(0.01)
         })
         controlA.on('pointerdown', ()=>{
             if(this.player.body.touching.down)
             this.player.setVelocityY(-380)
-            controlA.setScale(0.5)
+            controlA.setScale(0.6)
         })
         controlA.on('pointerup', ()=>{
-            controlA.setScale(0.6)
+            controlA.setScale(0.8)
         })
         
         //Update texts
